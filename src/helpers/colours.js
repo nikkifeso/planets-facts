025b4b1201d8f7a8cf099357afa9f9a773e1f119
Adenike Awofeso <adenikeawofeso@gmail.com>
2022-08-06 21:55:17 +0100

    Initial commit

---
+++ src/helpers/colours.js
@@ -0,0 +1,16 @@
+const Colours = {
+    white: "#FFFFFF",
+    black: "#070724",
+    darkGrey: "#38384F",
+    lightGrey: "#838391",
+    mercury: "#419EBB",
+    venus: "#EDA249",
+    earth: "#6F2ED6",
+    mars: "#D14C32",
+    jupiter: "#D83A34",
+    saturn: "#CD5120",
+    uranus: "#1EC2A4",
+    neptune: "#2D68F0"
+}
+
+export default Colours;
